6b7c76530032bb5014980a5e0a5a0f2ee44e37da
laurenz <laurenzschmidt214@gmail.com>
2025-05-28 18:32:47 +0200

    update bin/bash/05

--- bin/bash/05create_lfs_user.sh
+++ bin/bash/05create_lfs_user.sh
@@ -12,13 +12,18 @@ cd .. && cd .. && pwd
 # check if root started this
 __check_if_root
 
-groupadd lfs
-useradd -d /bin/bash -g lfs -m -k /dev/null lfs
-passwd lfs
+if  ! getent group "lfs" > /dev/null 2>&1; then
+  groupadd lfs
+fi
+
+if ! id "lfs" > /dev/null 2>&1; then
+  useradd -d /bin/bash -g lfs -m -k /dev/null lfs
+  passwd lfs
+fi
 #Grant lfs full access to all the directories under $LFS by making lfs the owner:
-chown -v lfs "${EXT4_MOUNT}/{usr{,/*},var,etc,tools}"
+chown -v lfs "${EXT4_MOUNT}"/{usr{,/*},var,etc,tools}
 case $(uname -m) in
-  x86_64) chown -v lfs $LFS/lib64 ;;
+  x86_64) chown -v lfs "${EXT4_MOUNT}"/lib64 ;;
 esac
 
-mkdir -pv "${EXT4_MOUNT}/{etc,var}" "${EXT4_MOUNT}/usr/{bin,lib,sbin}"
+mkdir -pv "${EXT4_MOUNT}"/{etc,var} "${EXT4_MOUNT}"/usr/{bin,lib,sbin}
